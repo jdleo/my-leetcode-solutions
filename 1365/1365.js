c5781cd1b205546d92f7662761b5900e22502128
John Leonardo <johnleonardo@csus.edu>
2020-03-31 18:48:07 -0700

    1365 completed

---
+++ 1365/1365.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = (nums) => {
+    // result
+    let res = [];
+    
+    // occurrence counts
+    let counts = Array(101).fill(0);
+    
+    // iterate thru nums
+    nums.forEach(num => {
+        // increment
+        counts[num]++;
+    })
+    
+    // iterate back thru
+    nums.forEach(num => {
+        // count of numbers smaller than current
+        let count = 0;
+        
+        // only iterate up to this number (all nums less than)
+        for (let i = 0; i < num; i++) {
+            // add occurrence to count
+            count += counts[i];
+        }
+        
+        // append count to res
+        res.push(count);
+    })
+    
+    // ret
+    return res;
+};
