28415cf5997924e4018f96df6beca23f0536bc5d
John Leonardo <johnleonardo@csus.edu>
2020-04-09 16:33:42 -0700

    122 completed

---
+++ 122/index.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = (prices) => {
+    // max profit
+    let profit = 0;
+
+    // current min
+    let curMin = Infinity;
+
+    // iterate thru prices
+    prices.forEach((price) => {
+        // check if this price is lower than min
+        if (price > curMin) {
+            // we can take profit today
+            profit += price - curMin;
+
+            // reset curMin
+            curMin = price;
+        } else {
+            // set new min
+            curMin = Math.min(curMin, price);
+        }
+    });
+
+    // ret
+    return profit;
+};
