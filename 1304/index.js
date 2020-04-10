f58974ad838d9c29fa7eb12cd30bc8e9fb75fde1
John Leonardo <johnleonardo@csus.edu>
2020-04-10 15:49:40 -0700

    1370 completed

--- 1304/index.js
+++ 1304/index.js
@@ -2,4 +2,31 @@
  * @param {number} n
  * @return {number[]}
  */
-var sumZero = function (n) {};
+const sumZero = (n) => {
+  // result (start with single 0)
+  let res = [0];
+
+  // counter for how many numbers weve added
+  let i = 0;
+
+  // negative and positive pointer
+  let neg = -1;
+  let pos = 1;
+
+  // iterate n - 1 times
+  for (let i = 0, j = 0; i < n - 1; i++) {
+    // i is even, use neg, i is odd, use pos
+    if (i % 2 === 0) {
+      // push neg to array, and decrement neg
+      res.push(neg--);
+    } else {
+      // push pos to array, and incremet pos
+      res.push(pos++);
+    }
+  }
+
+  // if n was even, we need to subtract the last number to the second to last
+  if (n % 2 === 0) res[n - 2] -= res[n - 1];
+
+  return res;
+};
