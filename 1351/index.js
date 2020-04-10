fef460eda08c0f311c99fafdad36c71ac8845029
John Leonardo <johnleonardo@csus.edu>
2020-04-10 14:57:52 -0700

    1351 completed

--- 1351/index.js
+++ 1351/index.js
@@ -2,6 +2,28 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var countNegatives = function(grid) {
-    
+const countNegatives = (grid) => {
+    // get sizes
+    const m = grid.length;
+    const n = grid[0].length;
+
+    // total number of negative numbers
+    let res = 0;
+
+    // iterate through rows
+    for (let i = 0; i < m; i++) {
+        // iterate backwards through cols 
+        // (until we see positive number, since sorted descending)
+        for (let j = n - 1; j >= 0; j--) {
+            // check if negative
+            if (grid[i][j] < 0) {
+                res++;
+            } else {
+                // don't go any further, since this row is sorted
+                break;
+            }
+        }
+    }
+
+    return res;
 };
