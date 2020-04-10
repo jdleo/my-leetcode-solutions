b1e633d9c3d4c4248e939dc4f900c7aafd53bca1
John Leonardo <johnleonardo@csus.edu>
2020-04-09 22:31:42 -0700

    1266 complete

--- 1266/index.js
+++ 1266/index.js
@@ -2,6 +2,29 @@
  * @param {number[][]} points
  * @return {number}
  */
-var minTimeToVisitAllPoints = function(points) {
-    
+const minTimeToVisitAllPoints = (points) => {
+    // running total distance
+    let total = 0;
+
+    // iterate until n - 1 in points
+    for (let i = 0; i < points.length - 1; i++) {
+        // add distance to total
+        total += distance(points[i], points[i+1]);
+    }
+
+    return total;
+};
+
+/**
+ * Helper method to calculate distance
+ * @param {number[]} p1
+ * @param {number[]} p2
+ * @return {number}
+ */
+const distance = (p1, p2) => {
+    // get coordinates
+    const [x1, y1, x2, y2] = [p1[0], p1[1], p2[0], p2[1]];
+
+    // return the max distance based on absolute distances between x/y
+    return Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1));
 };
