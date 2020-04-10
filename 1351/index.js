a00f5af67f70b41c4e209d3157ce8fbb36d313f4
John Leonardo <johnleonardo@csus.edu>
2020-04-10 15:04:13 -0700

    pretttier format 1351

--- 1351/index.js
+++ 1351/index.js
@@ -12,7 +12,7 @@ const countNegatives = (grid) => {
 
     // iterate through rows
     for (let i = 0; i < m; i++) {
-        // iterate backwards through cols 
+        // iterate backwards through cols
         // (until we see positive number, since sorted descending)
         for (let j = n - 1; j >= 0; j--) {
             // check if negative
