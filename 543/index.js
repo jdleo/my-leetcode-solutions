f01aeec514f68df7da5908d4464d04ca3bcc138c
John Leonardo <johnleonardo@csus.edu>
2020-04-11 00:55:20 -0700

    543 completed

--- 543/index.js
+++ 543/index.js
@@ -9,4 +9,30 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var diameterOfBinaryTree = function (root) {};
+const diameterOfBinaryTree = (root) => {
+  // diameter
+  let diameter = 0;
+
+  /**
+   * recursive helper function
+   * @param {TreeNode} node
+   */
+  const dfs = (node) => {
+    if (!node) return 0;
+
+    // get left diameter
+    const l = dfs(node.left);
+    const r = dfs(node.right);
+
+    // new diameter is either current, or left + right
+    diameter = Math.max(diameter, l + r);
+
+    // update largest path
+    return 1 + Math.max(l, r);
+  };
+
+  // perform dfs
+  dfs(root);
+
+  return diameter;
+};
