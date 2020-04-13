a1228b24214bfe562476f2f8b78b6222b442e25b
John Leonardo <johnleonardo@csus.edu>
2020-04-12 17:49:25 -0700

    905 completed

--- solutions/905/index.js
+++ solutions/905/index.js
@@ -3,15 +3,32 @@
  * @return {number[]}
  */
 const sortArrayByParity = (A) => {
-  const evens = [];
-  const odds = [];
-  A.forEach((num) => {
-    if (num % 2 === 0) {
-      evens.push(num);
-    } else {
-      odds.push(num);
-    }
-  });
+  // queue to hold odds
+  const queue = [];
 
-  return [...evens, ...odds];
+  // get size of A
+  const size = A.length;
+
+  // iterate thru nums
+  for (let i = 0; i < size; i++) {
+    // check even/odd
+    if (A[i] % 2 === 0) {
+      // check if queue is not empty
+      if (queue.length > 0) {
+        // this swap with first odd in queue
+        const f = queue.shift();
+        A[f.index] = A[i];
+        A[i] = f.val;
+
+        // push the odd to the back of queue
+        queue.push({ index: i, val: A[i] });
+      }
+    } else {
+      // add index and value to queue
+      queue.push({ index: i, val: A[i] });
+    }
+  }
+
+  // in-place
+  return A;
 };
