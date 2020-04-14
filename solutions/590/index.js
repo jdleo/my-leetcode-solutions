f88bf2b0d6a06a35bc02afabf93437e376c9b291
John Leonardo <johnleonardo@csus.edu>
2020-04-14 16:30:52 -0700

    590 completed

--- solutions/590/index.js
+++ solutions/590/index.js
@@ -1,30 +1,20 @@
-// typed interface for Node
-interface Node {
-    val: number;
-    children: Node[];
-}
-
-const postorder: (root: Node) => number[] = (root) => {
+const postorder = (root) => {
     // to hold path
-    const path: number[] = [];
-
+    const path = [];
     // call dfs
     dfs(root, path);
-
     return path;
 };
-
 // dfs helper function
-const dfs: (root: Node, path: number[]) => void = (root, path) => {
+const dfs = (root, path) => {
     // base case
-    if (!root) return;
-
+    if (!root)
+        return;
     // go through children in node
     for (const child of root.children) {
         // call dfs
         dfs(child, path);
     }
-
     // log to path, here (postorder)
     path.push(root.val);
 };
