8a66319db9c7c02ecba7f1bc53282660b6bc2c7f
John Leonardo <johnleonardo@csus.edu>
2020-04-13 23:50:30 -0700

    1381 completed

--- solutions/1381/index.js
+++ solutions/1381/index.js
@@ -2,14 +2,14 @@
  * @param {number} maxSize
  */
 var CustomStack = function (maxSize) {
-  // set max size
-  this.maxSize = maxSize;
+    // set max size
+    this.maxSize = maxSize;
 
-  // set initial size
-  this.size = 0;
+    // set initial size
+    this.size = 0;
 
-  // stack
-  this.stack = [];
+    // stack
+    this.stack = [];
 };
 
 /**
@@ -17,27 +17,27 @@ var CustomStack = function (maxSize) {
  * @return {void}
  */
 CustomStack.prototype.push = function (x) {
-  // make sure we're not at max size
-  if (this.size !== this.maxSize) {
-    // push as normal
-    this.stack.push(x);
-    // increment size
-    this.size++;
-  }
+    // make sure we're not at max size
+    if (this.size !== this.maxSize) {
+        // push as normal
+        this.stack.push(x);
+        // increment size
+        this.size++;
+    }
 };
 
 /**
  * @return {number}
  */
 CustomStack.prototype.pop = function () {
-  // check if empty to return -1
-  if (this.size === 0) {
-    return -1;
-  } else {
-    // decrement size and return pop()
-    this.size--;
-    return this.stack.pop();
-  }
+    // check if empty to return -1
+    if (this.size === 0) {
+        return -1;
+    } else {
+        // decrement size and return pop()
+        this.size--;
+        return this.stack.pop();
+    }
 };
 
 /**
@@ -46,9 +46,9 @@ CustomStack.prototype.pop = function () {
  * @return {void}
  */
 CustomStack.prototype.increment = function (k, val) {
-  // iterate from 0 to k (or size since thats as far we can go)
-  for (let i = 0; i < Math.min(k, this.size); i++) {
-    // increment at this index
-    this.stack[i] += val;
-  }
+    // iterate from 0 to k (or size since thats as far we can go)
+    for (let i = 0; i < Math.min(k, this.size); i++) {
+        // increment at this index
+        this.stack[i] += val;
+    }
 };
