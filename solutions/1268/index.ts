4337a62304e120626691c8ae437602edb4bd6885
John Leonardo <johnleonardo@csus.edu>
2020-04-17 10:51:55 -0700

    1268 done

--- solutions/1268/index.ts
+++ solutions/1268/index.ts
@@ -1,6 +1,29 @@
-function suggestedProducts(products: string[], searchWord: string): string[][] {
+function suggestedProducts(
+    products: string[],
+    searchWord: string
+): string[][] {
     // result
     const res: string[][] = [];
+
+    // sort products
+    products.sort();
+
+    // iterate thru search word
+    for (let i = 0; i < searchWord.length; i++) {
+        // get current search term
+        const phrase: string = searchWord.substr(0, i + 1);
+
+        // return products filtered by phrase
+        res.push(
+            products
+                .filter(
+                    (x) =>
+                        x >= phrase &&
+                        x <= phrase + 'zzzzzzzzzzzzzzzzz'
+                )
+                .slice(0, 3)
+        );
+    }
 
     return res;
 }
