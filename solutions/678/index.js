e1619cc2cc337066ce807193e78dfd16cdb3be81
John Leonardo <johnleonardo@csus.edu>
2020-04-16 23:00:03 -0700

    678 completed

--- solutions/678/index.js
+++ solutions/678/index.js
@@ -6,10 +6,12 @@ function checkValidString(s) {
         // if ( or *, increment balance, else decrement
         s.charAt(i) === '(' || s.charAt(i) === '*' ? balance++ : balance--;
         // if ever balanced, not even wildcards can save this
-        if (balance < 0) return false;
+        if (balance < 0)
+            return false;
     }
     // if perfectly balanced, this is a good string, dont need wildcards
-    if (balance === 0) return true;
+    if (balance === 0)
+        return true;
     // reset balance
     balance = 0;
     // right to left : * becomes )
@@ -17,7 +19,8 @@ function checkValidString(s) {
         // if ) or *, increment balance, else decrement
         s.charAt(i) === ')' || s.charAt(i) === '*' ? balance++ : balance--;
         // if ever balanced, not even wildcards can save this
-        if (balance < 0) return false;
+        if (balance < 0)
+            return false;
     }
     // never found any problems, cool
     return true;
