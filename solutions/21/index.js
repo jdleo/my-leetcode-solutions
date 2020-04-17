ad44ac2964eb222300759ee0a22a65584d9c4a18
John Leonardo <johnleonardo@csus.edu>
2020-04-16 23:00:15 -0700

    21 completed

--- solutions/21/index.js
+++ solutions/21/index.js
@@ -1,20 +1,12 @@
-// type interface for ListNode
-interface ListNode {
-    val: number;
-    next: ListNode;
-}
-
-function mergeTwoLists(l1: ListNode, l2: ListNode): ListNode {
+function mergeTwoLists(l1, l2) {
     // dummy list nodes for output
     const dummy_head = new ListNode(0);
     let dummy = dummy_head;
-
     // keep going while there are still nodes to be merged
     while (l1 || l2) {
         // get values of l1 and l2 (Infinity if null)
         const v1 = l1 ? l1.val : Infinity;
         const v2 = l2 ? l2.val : Infinity;
-
         // compare
         if (v1 < v2) {
             // add l1 to next
@@ -25,11 +17,9 @@ function mergeTwoLists(l1: ListNode, l2: ListNode): ListNode {
             dummy.next = new ListNode(v2);
             l2 = l2.next;
         }
-
         // move dummy forward
         dummy = dummy.next;
     }
-
     // return next in dummy node
     return dummy_head.next;
 }
