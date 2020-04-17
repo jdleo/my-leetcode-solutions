f2403135bc5976df29e59f734ff14f165ed2b928
John Leonardo <johnleonardo@csus.edu>
2020-04-16 23:17:30 -0700

    62 completed

--- solutions/62/index.js
+++ solutions/62/index.js
@@ -1,17 +1,14 @@
-function uniquePaths(m: number, n: number): number {
+function uniquePaths(m, n) {
     // fill nxm dp array with 0s
-    const dp: number[][] = [...Array(n)].map((a) => Array(m).fill(0));
-
+    const dp = [...Array(n)].map((a) => Array(m).fill(0));
     // fill top row with 1s (only one way to get here)
     for (let i = 0; i < m; i++) {
         dp[0][i] = 1;
     }
-
     // fill first column with 1s (only one way to get here)
     for (let j = 0; j < n; j++) {
         dp[j][0] = 1;
     }
-
     // iterate through 2d array
     for (let i = 1; i < n; i++) {
         for (let j = 1; j < m; j++) {
@@ -20,7 +17,6 @@ function uniquePaths(m: number, n: number): number {
             dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
     }
-
     // result is in last cell
     return dp[n - 1][m - 1];
 }
