a97d619249ac799d537d7cc22640e49082490599
John Leonardo <johnleonardo@csus.edu>
2020-04-18 01:07:45 -0700

    64 completed

--- solutions/64/index.js
+++ solutions/64/index.js
@@ -1,36 +1,28 @@
-function minPathSum(grid: number[][]): number {
+function minPathSum(grid) {
     // get m and n
-    const m: number = grid.length;
-    const n: number = grid[0].length;
-
+    const m = grid.length;
+    const n = grid[0].length;
     // dp array (filled with 0s initially)
-    const dp: number[][] = [...Array(m)].map((e) =>
-        Array(n).fill(0)
-    );
-
+    const dp = [...Array(m)].map((e) => Array(n).fill(0));
     // first value is just its own
     dp[0][0] = grid[0][0];
-
     // first row is just its own plus the left dp
     for (let j = 1; j < n; j++) {
         dp[0][j] = dp[0][j - 1] + grid[0][j];
     }
-
     // first colum is just its own plus top dp
     for (let i = 1; i < m; i++) {
         dp[i][0] = dp[i - 1][0] + grid[i][0];
     }
-
     // now go thru rest of dp array
     for (let i = 1; i < m; i++) {
         for (let j = 1; j < n; j++) {
             // take minimum path of left and top dp
             dp[i][j] =
                 grid[i][j] +
-                Math.min(dp[i - 1][j], dp[i][j - 1]);
+                    Math.min(dp[i - 1][j], dp[i][j - 1]);
         }
     }
-
     // result is in last sell
     return dp[m - 1][n - 1];
 }
