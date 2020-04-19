30ea8a601738327ce88f97733816e11fc252cff0
John Leonardo <johnleonardo@csus.edu>
2020-04-19 14:27:04 -0700

    33 completed

--- solutions/33/index.js
+++ solutions/33/index.js
@@ -1,41 +1,40 @@
-function search(nums: number[], target: number): number {
+function search(nums, target) {
     // pointers for binary search
-    let lo: number = 0;
-    let hi: number = nums.length - 1;
-
+    let lo = 0;
+    let hi = nums.length - 1;
     // go until pointers meet
     while (lo <= hi) {
         // get midpoint
-        const mid: number = Math.floor((hi + lo) / 2);
-
+        const mid = Math.floor((hi + lo) / 2);
         // check if target
         if (nums[mid] === target) {
             return mid;
-        } else if (nums[lo] <= nums[mid]) {
+        }
+        else if (nums[lo] <= nums[mid]) {
             // if true, this means left half IS sorted
-
             // check if target is in between lo and mid
             if (nums[lo] <= target && target <= nums[mid]) {
                 // search left half
                 hi = mid - 1;
-            } else {
+            }
+            else {
                 // search right half
                 lo = mid + 1;
             }
-        } else {
+        }
+        else {
             // left half NOT sorted
-
             // check if target is in between mid and hi
             if (nums[mid] <= target && target <= nums[hi]) {
                 // search right half
                 lo = mid + 1;
-            } else {
+            }
+            else {
                 // search left half
                 hi = mid - 1;
             }
         }
     }
-
     // not found
     return -1;
 }
