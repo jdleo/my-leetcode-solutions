cc5134fbeac70c4adbc75ed44f6b4edab4cc2b22
John Leonardo <johnleonardo@csus.edu>
2020-04-18 17:16:41 -0700

    566 completed

--- solutions/566/index.js
+++ solutions/566/index.js
@@ -1,27 +1,19 @@
-function matrixReshape(
-    nums: number[][],
-    r: number,
-    c: number
-) {
+function matrixReshape(nums, r, c) {
     // get mxn of current matrix
-    const m: number = nums.length;
-    const n: number = nums[0].length;
-
+    const m = nums.length;
+    const n = nums[0].length;
     // to check legality, check if these multiplied together are equal
-    if (m * n != r * c) return nums;
-
+    if (m * n != r * c)
+        return nums;
     // it's legal here, so create empty output array
-    const res: number[][] = [];
-
+    const res = [];
     // iterate r x c and increment a k variable
     for (let i = 0, k = 0; i < r; i++) {
         // push empty array
         res.push([]);
         for (let j = 0; j < c; j++, k++) {
-            // value at this index is kth value in nums
             res[i].push(nums[Math.floor(k / n)][k % n]);
         }
     }
-
     return res;
 }
