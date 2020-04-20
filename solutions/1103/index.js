59476745a93b1f646e955552fbf4025fbb7f1b63
John Leonardo <johnleonardo@csus.edu>
2020-04-20 08:52:13 -0700

    1103 completed

--- solutions/1103/index.js
+++ solutions/1103/index.js
@@ -1,32 +1,22 @@
-function distributeCandies(
-    candies: number,
-    num_people: number
-): number[] {
+function distributeCandies(candies, num_people) {
     // empty array of size num_people
-    const res: number[] = Array(num_people).fill(0);
-
+    const res = Array(num_people).fill(0);
     // candies to give (starts at 1)
-    let n: number = 1;
-
+    let n = 1;
     // pointer for loop
-    let i: number = 0;
-
+    let i = 0;
     // keep going until candies is 0
     while (candies > 0) {
         // give n candies to i'th person (circular indexing)
         res[i % num_people] += n;
-
         // subtract from candies (we gave them)
         candies -= n;
-
         // increase candies n + 1
         n++;
         i++;
     }
-
     // add rest of candies to last person we gave candies to
     // (this takes care of case that candies are negative at end)
     res[(i - 1) % num_people] += candies;
-
     return res;
 }
