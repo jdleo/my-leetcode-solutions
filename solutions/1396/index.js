956d74f85b5f7b9ec5cba7a2677c01e7a3087921
John Leonardo <johnleonardo@csus.edu>
2020-04-20 09:16:44 -0700

    1396 completed

--- solutions/1396/index.js
+++ solutions/1396/index.js
@@ -1,10 +1,4 @@
 class UndergroundSystem {
-    // data fields
-    private checkIns: {
-        [id: number]: { t: number; stationName: string };
-    };
-    private trips: { [route: string]: number[] };
-
     // class constructor
     constructor() {
         // check ins are of type {id : {checkIn time, station name}}
@@ -12,53 +6,34 @@ class UndergroundSystem {
         // trips are of type {route : [full elapsed times]}
         this.trips = {};
     }
-
-    checkIn(
-        id: number,
-        stationName: string,
-        t: number
-    ): void {
+    checkIn(id, stationName, t) {
         // log check in
         this.checkIns[id] = {
             t: t,
             stationName: stationName,
         };
     }
-
-    checkOut(
-        id: number,
-        stationName: string,
-        t: number
-    ): void {
+    checkOut(id, stationName, t) {
         // create route string
-        const route: string =
-            this.checkIns[id].stationName +
+        const route = this.checkIns[id].stationName +
             '=>' +
             stationName;
-
         // check if this route is not in trips obj yet
-        if (!(route in this.trips)) this.trips[route] = [];
+        if (!(route in this.trips))
+            this.trips[route] = [];
         // push elapsed time to route: trips
         this.trips[route].push(t - this.checkIns[id].t);
     }
-
-    getAverageTime(
-        startStation: string,
-        endStation: string
-    ): number {
+    getAverageTime(startStation, endStation) {
         // create route string
-        const route: string =
-            startStation + '=>' + endStation;
-
+        const route = startStation + '=>' + endStation;
         // running sum
-        let sum: number = 0;
-
+        let sum = 0;
         // iterate thru all elapsed trip times for this route
         for (let i = 0; i < this.trips[route].length; i++) {
             // add to running sum
             sum += this.trips[route][i];
         }
-
         // return avg
         return sum / this.trips[route].length;
     }
