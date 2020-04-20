344ccd967686348f499140511169dec4093bff74
John Leonardo <johnleonardo@csus.edu>
2020-04-20 16:29:06 -0700

    1287 completed

--- solutions/1287/index.js
+++ solutions/1287/index.js
@@ -1,14 +1,13 @@
-function findSpecialInteger(arr: number[]): number {
+function findSpecialInteger(arr) {
     // get 'window' size which is just 25% of array to int
-    const w: number = Math.ceil(arr.length * 0.25);
-
+    const w = Math.ceil(arr.length * 0.25);
     // just iterate thru array until window reaches end
     for (let i = 0; i + w < arr.length; i++) {
         // if number at beginning and end of window are same,
         // this is the special number
-        if (arr[i] === arr[i + w]) return arr[i];
+        if (arr[i] === arr[i + w])
+            return arr[i];
     }
-
     // never found one
     return -1;
 }
