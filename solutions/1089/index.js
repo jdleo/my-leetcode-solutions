f361ec022cb1c4449938a304bd63f34af93fba93
John Leonardo <johnleonardo@csus.edu>
2020-04-21 01:02:41 -0700

    1089 completed

--- solutions/1089/index.js
+++ solutions/1089/index.js
@@ -1,5 +1,5 @@
-function duplicateZeros(arr: number[]): void {
-    for (let i: number = 0; i < arr.length; i++) {
+function duplicateZeros(arr) {
+    for (let i = 0; i < arr.length; i++) {
         if (i === 0) {
             arr.splice(i, 0, 0);
             arr.pop();
