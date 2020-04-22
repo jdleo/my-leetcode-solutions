1fbd14f52bc12daf4bd7ba5fe57a116be9a5c52b
John Leonardo <johnleonardo@csus.edu>
2020-04-21 23:25:17 -0700

    o(n) solution

--- solutions/540/index.js
+++ solutions/540/index.js
@@ -1,10 +1,10 @@
-function singleNonDuplicate(nums: number[]): number {
+function singleNonDuplicate(nums) {
     // iterate thru nums by 2
     for (let i = 0; i < nums.length; i += 2) {
         // if this pair isn't equal, return the left
-        if (nums[i] !== nums[i + 1]) return nums[i];
+        if (nums[i] !== nums[i + 1])
+            return nums[i];
     }
-
     // none found
     return -1;
 }
