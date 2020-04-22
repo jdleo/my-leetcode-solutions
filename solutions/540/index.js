a7e1111212e03e1fc35ba278ca4cfc42f0782adc
John Leonardo <johnleonardo@csus.edu>
2020-04-22 00:09:29 -0700

    540 completed

--- solutions/540/index.js
+++ solutions/540/index.js
@@ -1,10 +1,37 @@
 function singleNonDuplicate(nums) {
-    // iterate thru nums by 2
-    for (let i = 0; i < nums.length; i += 2) {
-        // if this pair isn't equal, return the left
-        if (nums[i] !== nums[i + 1])
-            return nums[i];
+    // pointers for binary search
+    let lo = 0;
+    let hi = nums.length - 1;
+    // go until pointers meet
+    while (lo <= hi) {
+        // get midpoint
+        const mid = Math.floor((lo + hi) / 2);
+        // if reach first, or last, or left number not equal to right number
+        if (nums[mid - 1] !== nums[mid] &&
+            nums[mid] != nums[mid + 1])
+            return nums[mid];
+        // check if mid is equal to the left of mid
+        if (nums[mid] === nums[mid - 1]) {
+            if ((mid - 1) % 2 === 1) {
+                // amount of nums on left is odd, search left
+                hi = mid - 2;
+            }
+            else {
+                // search right
+                lo = mid + 1;
+            }
+        }
+        else {
+            if ((nums.length - 2 - mid) % 2 === 1) {
+                // amount of nums on right is odd, search right
+                lo = mid + 1;
+            }
+            else {
+                // search left
+                hi = mid - 1;
+            }
+        }
     }
-    // none found
+    // none found, i suppose
     return -1;
 }
--- solutions/540/index.ts
+++ solutions/540/index.ts
@@ -1,10 +1,39 @@
 function singleNonDuplicate(nums: number[]): number {
-    // iterate thru nums by 2
-    for (let i = 0; i < nums.length; i += 2) {
-        // if this pair isn't equal, return the left
-        if (nums[i] !== nums[i + 1]) return nums[i];
-    }
+    // pointers for binary search
+    let lo: number = 0;
+    let hi: number = nums.length - 1;
 
-    // none found
+    // go until pointers meet
+    while (lo <= hi) {
+        // get midpoint
+        const mid: number = Math.floor((lo + hi) / 2);
+
+        // if reach first, or last, or left number not equal to right number
+        if (
+            nums[mid - 1] !== nums[mid] &&
+            nums[mid] != nums[mid + 1]
+        )
+            return nums[mid];
+
+        // check if mid is equal to the left of mid
+        if (nums[mid] === nums[mid - 1]) {
+            if ((mid - 1) % 2 === 1) {
+                // amount of nums on left is odd, search left
+                hi = mid - 2;
+            } else {
+                // search right
+                lo = mid + 1;
+            }
+        } else {
+            if ((nums.length - 2 - mid) % 2 === 1) {
+                // amount of nums on right is odd, search right
+                lo = mid + 1;
+            } else {
+                // search left
+                hi = mid - 1;
+            }
+        }
+    }
+    // none found, i suppose
     return -1;
 }
