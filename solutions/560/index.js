7e9003882db06621a45357ab81ebac424fb1e40c
John Leonardo <johnleonardo@csus.edu>
2020-04-22 23:17:15 -0700

    560 completed

--- solutions/560/index.js
+++ solutions/560/index.js
@@ -3,31 +3,26 @@
  * @param {number} k
  * @return {number}
  */
-function subarraySum(nums: number[], k: number): number {
+function subarraySum(nums, k) {
     // running sum
-    let sum: number = 0;
-
+    let sum = 0;
     // amount of subarrays that add to K (res)
-    let count: number = 0;
-
+    let count = 0;
     // obj to store sums (and how many times they been seen)
-    let map: { [sum: number]: number } = { 0: 1 };
-
+    let map = { 0: 1 };
     // iterate thru arr
     for (let i = 0; i < nums.length; i++) {
         // add to running sum
         sum += nums[i];
-
         // check if sum - k is in map
         if (map[sum - k]) {
             // add the occurrence of that subarray to count
             count += map[sum - k];
         }
-
         // store sum in map
-        if (!map[sum]) map[sum] = 0;
+        if (!map[sum])
+            map[sum] = 0;
         map[sum]++;
     }
-
     return count;
 }
