4121a574020c4eb4c8597d4c4f5b6311c4792057
John Leonardo <johnleonardo@csus.edu>
2020-04-23 00:00:07 -0700

    929 completed

--- solutions/929/index.js
+++ solutions/929/index.js
@@ -1,31 +1,24 @@
-function numUniqueEmails(emails: string[]): number {
+function numUniqueEmails(emails) {
     // set of emails (to ensure uniqueness)
-    const set: Set<string> = new Set();
-
+    const set = new Set();
     // iterate thru emails
     for (const email of emails) {
         // put normalized email in set
         set.add(normalize(email));
     }
-
     // result is size of set
     return set.size;
 }
-
 // helper function to 'normalize' the email
-function normalize(email: string): string {
+function normalize(email) {
     // split email by @ sign
-    const s: string[] = email.split('@');
-
+    const s = email.split('@');
     // get username and domain
-    let [username, domain]: string[] = s;
-
+    let [username, domain] = s;
     // remove anything after +
     username = username.split('+')[0];
-
     // remove dots (they all go to same email)
     username = username.split('.').join('');
-
     // return normalized email
     return username + '@' + domain;
 }
