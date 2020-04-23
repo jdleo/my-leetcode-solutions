1a8e30c88229dddd5ecb3127b110d9ed4ed2d016
John Leonardo <johnleonardo@csus.edu>
2020-04-23 13:51:49 -0700

    1403 completed

--- solutions/1403/index.js
+++ solutions/1403/index.js
@@ -1,31 +1,25 @@
-function minSubsequence(nums: number[]): number[] {
+function minSubsequence(nums) {
     // result array
-    const res: number[] = [];
-
+    const res = [];
     // first, sort nums decreasing
-    nums.sort((a: number, b: number) => b - a);
-
+    nums.sort((a, b) => b - a);
     // get sum of elements
-    let sum: number = 0;
+    let sum = 0;
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
     }
-
     // get current sum
-    let current_sum: number = 0;
-
+    let current_sum = 0;
     // keep picking elements until it's greater than non included elements
     for (let i = 0; i < nums.length; i++) {
         // add to current sum and add number to res
         current_sum += nums[i];
         res.push(nums[i]);
-
         // if current sum is greater than non included elements, return res
         if (current_sum > sum - current_sum) {
             // break loop, we found our subsequence we need
             break;
         }
     }
-
     return res;
 }
