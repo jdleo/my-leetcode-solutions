c3748b83b9ccca1f2024c0d5bfaba65ce2cfd2d7
John Leonardo <johnleonardo@csus.edu>
2020-04-23 00:44:41 -0700

    1122 completed

--- solutions/1122/index.js
+++ solutions/1122/index.js
@@ -1,28 +1,19 @@
-var relativeSortArray = function (
-    arr1: number[],
-    arr2: number[]
-): number[] {
+var relativeSortArray = function (arr1, arr2) {
     // create object to store {num : index seen}
-    const obj: { [n: number]: number } = {};
-
+    const obj = {};
     // iterate thru arr2
     for (let i = 0; i < arr2.length; i++) {
         // put num and its index
         obj[arr2[i]] = i;
     }
-
     // sort arr1 based on appearance in arr2 (obj)
-    arr1.sort((a: number, b: number) => {
+    arr1.sort((a, b) => {
         // if either a or b not in obj, make it its number cubed + 1000
-        let val1: number =
-            a in obj ? obj[a] : a ** 3 + 1000;
-        let val2: number =
-            b in obj ? obj[b] : b ** 3 + 1000;
-
+        let val1 = a in obj ? obj[a] : a ** 3 + 1000;
+        let val2 = b in obj ? obj[b] : b ** 3 + 1000;
         // compare appearance a to b (only if both in arr2)
         return val1 - val2;
     });
-
     // return arr1 (sorted)
     return arr1;
 };
