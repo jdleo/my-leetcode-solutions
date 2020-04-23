cfe3de783458b2f3dad69b51a4598a5d356a2bc9
John Leonardo <johnleonardo@csus.edu>
2020-04-23 13:35:57 -0700

    201 completed

--- solutions/201/index.js
+++ solutions/201/index.js
@@ -1,17 +1,14 @@
-function rangeBitwiseAnd(m: number, n: number): number {
+function rangeBitwiseAnd(m, n) {
     // number of shifts we had to make
-    let i: number = 0;
-
+    let i = 0;
     // go until m and n are equal
     while (m !== n) {
         // right shift both m and n
         m >>= 1;
         n >>= 1;
-
         // that's 1 more shift
         i++;
     }
-
     // pad 0's on right of m by right shifting by i
     return (m <<= i);
 }
