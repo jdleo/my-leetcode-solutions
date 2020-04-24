54ccf30c765236a0e893dceb93924ca3d80599ba
John Leonardo <johnleonardo@csus.edu>
2020-04-24 00:09:08 -0700

    985 completed

--- solutions/985/index.js
+++ solutions/985/index.js
@@ -1,40 +1,32 @@
-function sumEvenAfterQueries(
-    A: number[],
-    queries: number[][]
-): number[] {
+function sumEvenAfterQueries(A, queries) {
     // result array
-    const res: number[] = [];
-
+    const res = [];
     // current sum of even numbers
-    let sum: number = 0;
-
+    let sum = 0;
     // first, add all even numbers to sum
     for (let i = 0; i < A.length; i++) {
-        if (A[i] % 2 === 0) sum += A[i];
+        if (A[i] % 2 === 0)
+            sum += A[i];
     }
-
     // then, go thru queries
     for (let i = 0; i < queries.length; i++) {
         // destructure query
-        const [val, index]: number[] = queries[i];
-
+        const [val, index] = queries[i];
         // check if adding val would make it even
         if ((val + A[index]) % 2 == 0) {
             // if even, add val, if odd, add new + old
             A[index] % 2 == 0
                 ? (sum += val)
                 : (sum += val + A[index]);
-        } else {
+        }
+        else {
             // if even, subtract old, if odd, do nothing
             A[index] % 2 == 0 ? (sum -= A[index]) : false;
         }
-
         // add val to old
         A[index] += val;
-
         // push current sum to res
         res.push(sum);
     }
-
     return res;
 }
