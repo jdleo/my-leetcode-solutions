cf53c60d5ba4d55495549fca52f0282db21da634
John Leonardo <johnleonardo@csus.edu>
2020-04-25 13:06:02 -0700

    1400 completed

--- solutions/1400/index.js
+++ solutions/1400/index.js
@@ -1,25 +1,22 @@
-function canConstruct(s: string, k: number): boolean {
+function canConstruct(s, k) {
     // base case (s is less than k in length)
-    if (s.length < k) return false;
-
+    if (s.length < k)
+        return false;
     // our number to representing all 26 lowercase characters (each bit)
-    let x: number = 0;
-
+    let x = 0;
     // iterate thru string
     for (let i = 0; i < s.length; i++) {
         // flip bit corresponding to this lowercase char
         x ^= 1 << (s.charCodeAt(i) - 97);
     }
-
     // count set bits
-    let c: number = 0;
+    let c = 0;
     while (x > 0) {
         // inc c if rightmost bit is 1
         c += x & 1;
         // right shift
         x >>= 1;
     }
-
     // check if number of odds is less or equal to k
     return c <= k;
 }
