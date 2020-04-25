c1ee829ea5f5cb48d9af6599aa0a7da33507aa44
John Leonardo <johnleonardo@csus.edu>
2020-04-25 00:06:23 -0700

    146 completed

--- solutions/146/index.js
+++ solutions/146/index.js
@@ -1,14 +1,39 @@
-class LRUCache {
+class LRUCache extends Map {
     // constructor
     constructor(capacity) {
+        // call parent constructor
+        super();
         // set fields
         this.capacity = capacity;
-        this.obj = {};
-        this.queue = Array(capacity).fill(NaN);
     }
+    /**
+     * override built-in get() method to update number order on get()
+     * @override
+     */
     get(key) {
-        // if key is in obj, return val
-        return key in this.obj ? this.obj[key] : -1;
+        // check if it doesn't contain key
+        if (!this.has(key))
+            return -1;
+        // delete key, and re-add to add to end of map
+        const val = super.get(key);
+        this.delete(key);
+        this.set(key, val);
+        // return value
+        return val;
     }
-    put(key, value) { }
+    put(key, value) {
+        // check if we're below capacity, or just updating existing key
+        if (this.size < this.capacity || this.has(key)) {
+            // call get() method to update order
+            this.get(key);
+            // insert as normal
+            this.set(key, value);
+        }
+        else if (this.size > 0) {
+            // we checked if size is 0, so we have something to delete
+            this.delete(this.keys().next().value);
+            // now, add new key:value
+            this.set(key, value);
+        }
+    }
 }
--- solutions/146/index.ts
+++ solutions/146/index.ts
@@ -1,21 +1,44 @@
-class LRUCache {
-    // class attributes
+class LRUCache extends Map {
+    // class attribute
     private capacity: number;
-    private obj: { [key: number]: number };
-    private queue: number[];
 
     // constructor
     constructor(capacity: number) {
+        // call parent constructor
+        super();
         // set fields
         this.capacity = capacity;
-        this.obj = {};
-        this.queue = Array(capacity).fill(NaN);
     }
 
+    /**
+     * override built-in get() method to update number order on get()
+     * @override
+     */
     get(key: number): number {
-        // if key is in obj, return val
-        return key in this.obj ? this.obj[key] : -1;
+        // check if it doesn't contain key
+        if (!this.has(key)) return -1;
+
+        // delete key, and re-add to add to end of map
+        const val: number = super.get(key);
+        this.delete(key);
+        this.set(key, val);
+
+        // return value
+        return val;
     }
 
-    put(key: number, value: number): void {}
+    put(key: number, value: number): void {
+        // check if we're below capacity, or just updating existing key
+        if (this.size < this.capacity || this.has(key)) {
+            // call get() method to update order
+            this.get(key);
+            // insert as normal
+            this.set(key, value);
+        } else if (this.size > 0) {
+            // we checked if size is 0, so we have something to delete
+            this.delete(this.keys().next().value);
+            // now, add new key:value
+            this.set(key, value);
+        }
+    }
 }
