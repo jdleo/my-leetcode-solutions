87b27427a4011304d477ab381516b1eae12c232d
John Leonardo <johnleonardo@csus.edu>
2020-04-25 02:01:52 -0700

    917 completed

--- solutions/917/index.js
+++ solutions/917/index.js
@@ -1,36 +1,33 @@
-function reverseOnlyLetters(S: string): string {
+function reverseOnlyLetters(S) {
     // left and right pointers
-    let l: number = 0;
-    let r: number = S.length - 1;
-
+    let l = 0;
+    let r = S.length - 1;
     // result string array
-    const s: string[] = S.split('');
-
+    const s = S.split('');
     // go until pointers meet
     while (l <= r) {
         // check char code is not alphanumeric
         if (!isAlpha(S.charCodeAt(l))) {
             // move left pointer
             l++;
-        } else if (!isAlpha(S.charCodeAt(r))) {
+        }
+        else if (!isAlpha(S.charCodeAt(r))) {
             // move right pointer
             r--;
-        } else {
+        }
+        else {
             // swap these characters
-            const tmp: string = s[l];
+            const tmp = s[l];
             s[l] = s[r];
             s[r] = tmp;
-
             // move both pointers forward
             l++;
             r--;
         }
     }
-
     return s.join('');
 }
-
 // helper method to determine if char code is alphanumeric
-function isAlpha(c: number): boolean {
+function isAlpha(c) {
     return (c >= 97 && c <= 122) || (c >= 65 && c <= 90);
 }
