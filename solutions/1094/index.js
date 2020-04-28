0c5ca980e74430601f8614ea52974f119c7ab72f
John Leonardo <johnleonardo@csus.edu>
2020-04-28 14:31:07 -0700

    1094

---
+++ solutions/1094/index.js
@@ -0,0 +1,9 @@
+var carPooling = function (trips, capacity) {
+    // iterate thru trips
+    for (let i = 0; i < trips.length; i++) {
+        // num_passengers, start_location, end_location
+        const [num, start, end] = trips[i];
+    }
+    // no problems, so it was successful carpool
+    return true;
+};
