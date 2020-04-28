514eb98c17a211bce180250683d361ee4b33be3c
John Leonardo <johnleonardo@csus.edu>
2020-04-28 14:44:02 -0700

    1137 completed

--- solutions/1137/index.js
+++ solutions/1137/index.js
@@ -1,24 +1,22 @@
-export {};
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // memo map for holding trib numbers
-const memo: Map<number, number> = new Map();
+const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
 memo.set(2, 1);
-
-function tribonacci(n: number): number {
+function tribonacci(n) {
     // check if n is in memo
     if (memo.has(n)) {
         // return trib value in memo
         return memo.get(n);
-    } else {
+    }
+    else {
         // we need to compute n and set in memo
-        const trib: number =
-            tribonacci(n - 1) +
+        const trib = tribonacci(n - 1) +
             tribonacci(n - 2) +
             tribonacci(n - 3);
         memo.set(n, trib);
-
         // return what we just computed from memo
         return memo.get(n);
     }
