a580dbe42ad42cc9c9ffc6c7340cf2f2482a57e8
John Leonardo <johnleonardo@csus.edu>
2020-05-03 01:19:12 -0700

    383 completed

--- solutions/383/index.js
+++ solutions/383/index.js
@@ -1,10 +1,6 @@
-function canConstruct(
-    ransomNote: string,
-    magazine: string
-): boolean {
+function canConstruct(ransomNote, magazine) {
     // map to count chars in magazine
-    const map: { [char: string]: number } = {};
-
+    const map = {};
     // iterate thru map
     for (let i = 0; i < magazine.length; i++) {
         // increment char count in map
@@ -12,7 +8,6 @@ function canConstruct(
             map[magazine.charAt(i)] = 0;
         map[magazine.charAt(i)]++;
     }
-
     // iterate thru ransom note
     for (let i = 0; i < ransomNote.length; i++) {
         // if char not in map, or count is <= 0, break ret false
@@ -23,7 +18,6 @@ function canConstruct(
             return false;
         }
     }
-
     // no problems
     return true;
 }
