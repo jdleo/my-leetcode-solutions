a95016cd9a79ccba036ee0cfbb454361f5aa3fcc
John Leonardo <johnleonardo@csus.edu>
2020-07-07 20:09:56 -0500

    1480 completed

--- solutions/1480/index.ts
+++ solutions/1480/index.ts
@@ -1,1 +1,13 @@
-function runningSum(nums: number[]): number[] {}
+function runningSum(nums: number[]): number[] {
+    // variable for running sum
+    let sum: number = 0;
+
+    // iterate thru nums
+    for (let i = 0; i < nums.length; i++) {
+        // add to running sum and make i'th position running sum
+        sum += nums[i];
+        nums[i] = sum;
+    }
+
+    return nums;
+}
