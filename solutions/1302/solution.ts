0634022d4b62ae4d9c5922e05be1bcbd4435ddd7
John Leonardo <johnleonardo@csus.edu>
2020-07-11 19:24:00 -0500

    1302 completed

---
+++ solutions/1302/solution.ts
@@ -0,0 +1,34 @@
+function deepestLeavesSum(root: TreeNode | null): number {
+    // total sum
+    let sum: number = 0;
+
+    // helper function to find max depth
+    function maxDepth(root: TreeNode | null): number {
+        // base case
+        if (!root) return 0;
+
+        // return max of left and right subtrees
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    // get max depth of tree
+    const depth: number = maxDepth(root);
+
+    // dfs helper function
+    function dfs(root: TreeNode | null, level: number): void {
+        // base case
+        if (!root) return;
+
+        // check if we're at deepest level
+        if (level === depth) sum += root.val;
+
+        // go left, right
+        dfs(root.left, level + 1);
+        dfs(root.right, level + 1);
+    }
+
+    // start dfs
+    dfs(root, 1);
+
+    return sum;
+}
