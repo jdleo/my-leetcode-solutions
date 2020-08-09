a22114eeb02c040b338b0052c0aa7011dfcadacb
John Leonardo <johnleonardo@csus.edu>
2020-08-09 18:39:01 -0500

    started 654

---
+++ solutions/654/solution.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// interface for TreeNode
+interface TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+}
+
+function constructMaximumBinaryTree(nums: number[]): TreeNode | null {
+    // recursive helper function
+}
