71c7f2b279d5d5366be8eb7d6ca0fe247f89c098
John Leonardo <j@jdleo.me>
2020-08-30 21:27:45 -0500

    completed 535

---
+++ solutions/1534/solution.ts
@@ -0,0 +1,26 @@
+function countGoodTriplets(
+    arr: number[],
+    a: number,
+    b: number,
+    c: number
+): number {
+    // number of good triplets
+    let n = 0;
+
+    // iterate thru array n^3
+    for (let k = 0; k < arr.length; k++) {
+        for (let j = 0; j < k; j++) {
+            for (let i = 0; i < j; i++) {
+                if (
+                    Math.abs(arr[i] - arr[j]) <= a &&
+                    Math.abs(arr[j] - arr[k]) <= b &&
+                    Math.abs(arr[i] - arr[k]) <= c
+                ) {
+                    n++;
+                }
+            }
+        }
+    }
+
+    return n;
+}
