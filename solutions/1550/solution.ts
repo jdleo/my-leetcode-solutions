53e070a4050a3c84a5658fcd400e512e14162843
John Leonardo <j@jdleo.me>
2020-09-25 10:40:49 -0500

    completed 1550

---
+++ solutions/1550/solution.ts
@@ -0,0 +1,13 @@
+function threeConsecutiveOdds(arr: number[]): boolean {
+    // current number of odds
+    let odds = 0;
+    // iterate thru arr
+    for (let i = 0; i < arr.length; i++) {
+        // if even, reset count
+        if (arr[i] % 2 === 0) odds = 0;
+        // it's odd, increment and check if we have 3 consecutive
+        else if (++odds === 3) return true;
+    }
+    // never found 3 consecutive odds
+    return false;
+}
