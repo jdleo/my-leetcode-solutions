8630c031130cb7a74587b7aec9ff894b64d2889b
John Leonardo <j@jdleo.me>
2020-09-29 13:03:27 -0500

    completed 136

--- solutions/136/solution.ts
+++ solutions/136/solution.ts
@@ -1,7 +1,7 @@
 function singleNumber(nums: number[]): number {
     // result
     let res = 0;
-    // iterate thru nums
+    // iterate thru nums, and xor each num with res
     for (let i = 0; i < nums.length; i++) res ^= nums[i];
     return res;
 }
